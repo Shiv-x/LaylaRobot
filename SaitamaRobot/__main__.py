20ac83674b22bd4a0512017e28ec94b0079c182f
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2020-09-26 08:49:36 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -161,7 +161,7 @@ def start(update: Update, context: CallbackContext):
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup([
                   [InlineKeyboardButton(text="Add Jassica to your group", url="t.me/{}?startgroup=true".format(context.bot.username))],
-                  [InlineKeyboardButton(text="Source Code", url="https://github.com/QueenArzoo/JassicaRobot.git")]]))
+                  [InlineKeyboardButton(text="Source Code", url="https://github.com/QueenArzoo/JassicaRobot")]]))
     else:
         update.effective_message.reply_text("Yo, whadup?")
 
