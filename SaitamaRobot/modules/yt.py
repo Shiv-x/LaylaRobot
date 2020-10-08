5d4478b582bf09a8eb60eb7dac7b5972200fe947
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2020-10-08 14:20:52 +0530

    Add files via upload

---
+++ SaitamaRobot/modules/yt.py
@@ -0,0 +1,5 @@
+__help__ = """
+ - /yt <text>: perform a youtube search
+ - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+"""
+__mod_name__ = "Youtube"
