c7df4ce1ea0cfdcce17155c8db4e63e5e32c389e
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2020-12-20 10:45:57 +0530

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -92,6 +92,7 @@ if ENV:
     SUPPORT_CHAT = os.environ.get('SUPPORT_CHAT', None)
     SPAMWATCH_SUPPORT_CHAT = os.environ.get('SPAMWATCH_SUPPORT_CHAT', None)
     SPAMWATCH_API = os.environ.get('SPAMWATCH_API', None)
+    REDIS_URL = os.environ.get('REDIS_URL', None)
 
     try:
         BL_CHATS = set(int(x) for x in os.environ.get('BL_CHATS', "").split())
