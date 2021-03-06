d77fe7d876605a34aadcdd1b6fca6a128b192956
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-03-06 04:10:45 -0800

    Delete SaitamaRobot directory

--- SaitamaRobot/modules/yt.py
+++
@@ -1,5 +0,0 @@
-__help__ = """
- - /yt <text>: perform a youtube search
- - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
-"""
-__mod_name__ = "Youtube"
