1038562e4464f9e0e385477ac1593b05aebf545e
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-03-06 09:52:55 -0800

    Create help.py

---
+++ LaylaRobot/modules/help.py
@@ -0,0 +1,58 @@
+import logging
+from LayalRobot.Config import Messages as tr
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+logging.basicConfig(level=logging.INFO)
+
+@Client.on_message(filters.private & filters.incoming & filters.command(['start']))
+def _start(client, message):
+    client.send_message(message.chat.id,
+        text=tr.START_MSG.format(message.from_user.first_name, message.from_user.id),
+        parse_mode="markdown",
+        reply_to_message_id=message.message_id
+        )
+
+
+@Client.on_message(filters.private & filters.incoming & filters.command(['help']))
+def _help(client, message):
+    client.send_message(chat_id = message.chat.id,
+        text = tr.HELP_MSG[1],
+        parse_mode="markdown",
+        disable_notification = True,
+        reply_markup = InlineKeyboardMarkup(map(1)),
+        reply_to_message_id = message.message_id
+    )
+
+help_callback_filter = filters.create(lambda _, __, query: query.data.startswith('help+'))
+
+@Client.on_callback_query(help_callback_filter)
+def help_answer(client, callback_query):
+    chat_id = callback_query.from_user.id
+    message_id = callback_query.message.message_id
+    msg = int(callback_query.data.split('+')[1])
+    client.edit_message_text(chat_id=chat_id,    message_id=message_id,
+        text=tr.HELP_MSG[msg],    reply_markup=InlineKeyboardMarkup(map(msg))
+    )
+
+
+def map(pos):
+    if(pos==1):
+        button = [
+            [InlineKeyboardButton(text = '-->', callback_data = "help+2")]
+        ]
+    elif(pos==len(tr.HELP_MSG)-1):
+        url = "https://github.com/viperadnan-git/force-subscribe-telegram-bot/issues/new"
+        button = [
+            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/ViperCommunity")],
+            [InlineKeyboardButton(text = 'Feature Request & Issues', url=url)],
+            [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
+        ]
+    else:
+        button = [
+            [
+                InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}"),
+                InlineKeyboardButton(text = '-->', callback_data = f"help+{pos+1}")
+            ],
+        ]
+    return button
