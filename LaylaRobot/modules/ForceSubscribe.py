1c17a31328bc8a005d678644451d0aeb24ad643d
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-04-23 12:53:06 -0700

    Update ForceSubscribe.py

--- LaylaRobot/modules/ForceSubscribe.py
+++ LaylaRobot/modules/ForceSubscribe.py
@@ -11,7 +11,7 @@ from pyrogram.errors.exceptions.bad_request_400 import (
 from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboardMarkup
 
 from LaylaRobot import DRAGONS as SUDO_USERS
-from LaylaRobot import pgram
+from LaylaRobot import pbot
 from LaylaRobot.modules.sql import forceSubscribe_sql as sql
 
 logging.basicConfig(level=logging.INFO)
