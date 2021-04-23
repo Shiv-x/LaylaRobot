eeda5acd92496c0aa63e7082f2feb9ed618b697c
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-04-23 12:57:39 -0700

    Update banall.py

--- LaylaRobot/modules/banall.py
+++ LaylaRobot/modules/banall.py
@@ -7,7 +7,7 @@ import sys
 import aiohttp
 import regex
 from aiohttp import ClientSession
-from LaylaRobot import DRAGONS, TOKEN, pgram
+from LaylaRobot import DRAGONS, TOKEN, pbot
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
