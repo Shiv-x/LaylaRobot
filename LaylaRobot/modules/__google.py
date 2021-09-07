411830a87b2a225e443eab592d5bb5d93a6b7774
Okek <87618530+skuyykek69@users.noreply.github.com>
2021-09-08 04:02:24 +0700

    typo fixes

--- LaylaRobot/modules/__google.py
+++ LaylaRobot/modules/__google.py
@@ -286,6 +286,6 @@ __help__ = """
  ❍ /github <username>*:* Get information about a GitHub user.
  ❍ /country <country name>*:* Gathering info about given country
  ❍ /imdb <Movie name>*:* Get full info about a movie with imdb.com
- ❍ Yone <query>*:* Yone answers the query
-  💡Ex: `Yone where is India?`
+ ❍ Layla <query>*:* Layla answers the query
+  💡Ex: `Layla where is India?`
 """
